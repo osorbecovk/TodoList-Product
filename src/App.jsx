856596c9c69dbe7307aeb41fa77b34d0ea421f35
osorbecovk <osorbecov@gmail.com>
2025-03-12 09:55:56 +0600

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,27 @@ import './App.css'
 import Header from './Components/Header'
 import Footer from './Components/Footer'
 import Hero from './Components/Hero'
+import About from "./Components/About"
+import Services from "./Components/Services"
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
+   
+  let routers = [
+    { id: 1, link: "/", element: <Hero /> },
+    { id: 2, link: "/about", element: <About /> },
+    { id: 3, link: "/services", element: <Services /> }
+  ]
 
   return (
     <div className='App'>
-    <Header/>
-    <Hero />
-    <Footer />
+      <Header />
+      <Routes>
+        {routers.map((el) => (
+          <Route key={el.id} path={el.link} element={el.element} />
+        ))}
+      </Routes>
+      <Footer />
     </div>
   )
 }
